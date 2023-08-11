70cfd92098cebaa4ca312376c28d9149db8ec24f
NikitaGory <nickgoryachek@gmail.com>
2023-08-11 16:07:42 +0200

    modal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-new WOW().init();
+window.addEventListener("DOMContentLoaded", () => {
+    const openModalBtn = document.querySelector(".open_modal");
+    const closeModalBtn = document.querySelector(".close_modal");
+    const modal = document.querySelector(".overlay");
+  
+    openModalBtn.addEventListener("click", () => {
+      modal.classList.add("show");
+    });
+    closeModalBtn.addEventListener("click", () => {
+      modal.classList.remove("show");
+    });
+  });
